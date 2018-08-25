f1c215a0f8bab3448edc29328ed42714762a4613
student <student@101-09>
2018-08-25 17:11:35 +0300

    End Class

--- exam/Program.cs
+++ exam/Program.cs
@@ -1,31 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace exam
 {
-    class Calculor
-    {
-        int x;
-        int y;
-        public int Plus(int x, int y)
-        {
-            return x + y;
-        }
-        public double Delenie(int x, int y)
-        {
-            return x / y;
-        }
 
-    }
+
+
     class Program
     {
+public class Calculor
+        {
+            public int x;
+            public int y;
+            public Calculor(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public Calculor()
+            {
+                this.x = 0;
+                this.y = 0;
+            }
+            public int Plus()
+            {
+                return x + y;
+            }
+            public double Delenie()
+            {
+                return x / y;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine();
-            
-        }
+  
+        Console.WriteLine("Введите первое число:  ");
+            int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("\nВведите первое число:  ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Calculor calc = new Calculor(x, y);
+          Console.WriteLine("Результат" + calc.Delenie());
+    }
     }
 }
+
+
