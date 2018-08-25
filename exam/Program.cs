5bd8c6d013630e6aac614e923c1b549d21a026f5
student <student@101-09>
2018-08-25 16:46:21 +0300

    Class Calculor

--- exam/Program.cs
+++ exam/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace exam
 {
+    class Calculor
+    {
+        int x;
+        int y;
+        public int Plus(int x, int y)
+        {
+            return x + y;
+        }
+        public double Delenie(int x, int y)
+        {
+            return x / y;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine();
+            
         }
     }
 }
